d0f18b1795bd9bb6458c0b0aa2e2f35d6fa251d2
Dmitri Don <fotkin@gmail.com>
2026-05-29 17:12:33 +0000

    Libs upgraded

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule PhoenixKitCatalogue.MixProject do
     [
       quality: ["format", "credo --strict", "dialyzer"],
       "quality.ci": ["format --check-formatted", "credo --strict", "dialyzer"],
-      precommit: ["compile", "quality"]
+      precommit: ["compile --force --warnings-as-errors", "deps.unlock --check-unused", "quality.ci"]
     ]
   end
 
